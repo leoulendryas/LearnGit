d5f631fe9cef9491fb5b851f12b6298b83653d2c
leoulendryas <leoulendryas@gmail.com>
2024-04-13 21:39:05 +0300

    branch test

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -4,7 +4,7 @@ export default function Notifications() {
   return (
     <>
       <h1>Notifications dashboard</h1>
-      <Link href='/complex-dashboard/archived'>archived!</Link>
+      <Link href='/complex-dashboard/archived'>archived</Link>
     </>
   )
 }
